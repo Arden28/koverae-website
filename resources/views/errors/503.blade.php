6de9fe7325929204a69c0204e355a5b72bcaac3d
Arden28 <laudbouetoumoussa@gmail.com>
2024-11-15 05:49:01 +0100

    Updated the maintenance page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,95 @@
-@extends('errors::minimal')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Koverae Ltd">
+    <meta name="description" content="Koverae is a dynamic startup focused on developing cutting-edge solutions to help businesses optimize their operations. With a commitment to innovation, Koverae provides tools and platforms designed to drive growth, improve efficiency, and enhance user experience, all while aiming to simplify complex processes for businesses of all sizes." />
+    <meta name="keywords" content="ERP, SaaS, Software, All-in-One Business Management, Financial Tracking, Guest and Tenant Portal, Nairobi Business Management, Business Management Tools, Rental Property Software, Tenant and Owner Solutions, Nairobi, Kenya Software, Kenya" />
+    <link rel="shortcut icon" href="assets/images/logo/favicon.ico">
+    <title>Koverae - 🔨🚧🧱</title>
 
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __('Service Unavailable'))
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "assets/images/logo/logo-black.png"
+        }
+    </script>
+    <!-- Open Graph -->
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Business Management Software - Koverae">
+    <meta property="og:description" content="All-in-one business management platform" />
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.koverae.com">
+    <!-- Open Graph -->
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-HJ1L2Y7QS6"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-HJ1L2Y7QS6');
+    </script>
+    <!-- Google tag End (gtag.js) -->
+
+    <!-- Meta Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '291287213552434');
+        fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=291287213552434&ev=PageView&noscript=1" />
+    </noscript>
+    <!-- End Meta Pixel Code -->
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+
+    <style>
+        .under-dev{
+            padding: 80px 32px 80px 32px;
+        }
+
+        .under-dev .k-primary {
+            background-color: #026469;
+            outline: none;
+            color: white;
+        }
+        .under-dev .k-primary:hover{
+            background-color: #034f53;
+            color: white;
+        }
+    </style>
+</head>
+<body class="overflow-hidden">
+    <main class="under-dev">
+        <div class="row">
+            <div class="p-4 col-md-6">
+                <div class="logo d-flex">
+                    <img src="assets/images/logo/logo.png" height="50px" alt="Koverae Logo">
+                </div>
+                <h1 class="mt-4">Our website is evolving to bring you something extraordinary</h1>
+                <p class="mt-3 fs-5">We’ll be launching soon, but in the meantime, explore our exciting new Ndako product, a glimpse of the future awaits!</p>
+                <a href="https://www.ndako.koverae.com?medium=dotcom" class="p-2 btn k-primary fw-bold">Explore Ndako (bêta) – Discover the future</a>
+            </div>
+            <div class="col-md-6">
+                <img src="assets/images/illustrations/under-development.svg" height="" alt="">
+            </div>
+        </div>
+    </main>
+    <link rel="stylesheet" href="assets/css/js.css">
+</body>
+</html>
